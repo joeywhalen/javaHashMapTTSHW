abf65d6a157c79709f378a2223ef041c0c5f103a
Joey Whalen <jtktwhalen@gmail.com>
2021-01-30 23:27:00 -0500

    update Main to create HashMap Autos

--- src/javaHashMapTTSHW/Main.java
+++ src/javaHashMapTTSHW/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Create Hash Map for auto vehicles
-
+		HashMap<String, String> Autos = new HashMap<String, String>();// create HashMap Object called Autos
 	}
 
 }
